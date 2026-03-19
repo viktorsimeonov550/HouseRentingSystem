8d7b04d2c1a9ac24b9cbc0f699c82021b7a080e4
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2026-03-19 14:25:53 +0200

    new update

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace House_renting_system_Project.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
---
+++ HouseRentingSystem/Models/HousesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace House_renting_system_Project.Models
+{
+    public class HousesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
