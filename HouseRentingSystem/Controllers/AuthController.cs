74ea6c922a27343929632ba41fe286d0c51207f2
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2026-03-31 12:26:55 +0300

    new

--- HouseRentingSystem/Controllers/AuthController.cs
+++ HouseRentingSystem/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using House_renting_system_Project..Data.Entities;
+﻿using House_renting_system_Project.Data.Data.Entities;
 using House_renting_system_Project.Models.Auth;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -21,13 +21,63 @@ namespace House_renting_system_Project.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                //ModelState.AddModelError();
+                return View(model);
+            }
+
+            var result = await userManager.CheckPasswordAsync(user, model.Password);
+            if (result == true)
+            {
+                await signInManager.SignInAsync(user, model.RememberMe);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError("", "User already exists");
+                return View(model);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.Username
+            };
+
+            var result = await userManager.CreateAsync(newUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
         }
     }
 }
